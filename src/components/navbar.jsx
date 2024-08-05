d74cfea9b6d47399af8d177ef5e41c66875c86e7
anirban2005143a <23je0104@iitism.ac.in>
2024-08-05 21:27:05 +0530

    dfrg

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,27 +5,31 @@ export default function Navbar
     () {
     return (
         <>
-            <header className="text-gray-600 body-font">
-                <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-                    <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-                    {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-yellow-500 rounded-full" viewBox="0 0 24 24">
-                        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-                    </svg> */}
-                    <span className="ml-3 text-xl">Buffered Reader</span>
-                    </a>
-                    <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-                    <a className="mr-5 hover:text-gray-900">Achievements</a>
-                    <a className="mr-5 hover:text-gray-900">Past-Editions</a>
-                    <a className="mr-5 hover:text-gray-900">About Us</a>
-                    <a className="mr-5 hover:text-gray-900">Fourth Link</a>
+             <header className="w-full sticky top-0 text-gray-600 body-font">
+                <div className="container w-full flex flex-wrap p-5 flex-col md:flex-row items-center md:justify-between ">
+                    <div>                    
+                            <a className=" flex title-font font-medium items-center text-gray-100 mb-4 md:mb-0 " href="/">
+                                <img className='pl-2 h-10 rounded-lg' src="..\public\logo.jpg" alt="oops" />
+
+                                <span className="ml-3 text-2xl">Buffered Reader</span>
+                            </a>
+                    </div>
+
+                    <nav className=" flex flex-wrap items-center text-base justify-between ">
+
+                        <a className="mr-16 scroll-m-2 text-gray-100 hover:underline underline-offset-8" href="#search">Blogs</a>                       
+                        <a className="mr-16 text-gray-100 hover:underline underline-offset-8" href="#search">Articles</a>
+                        <a className="mr-16 text-gray-100 hover:underline underline-offset-8" href="#search">Old_Versions</a>
+                        <a className="mr-16 text-gray-100 hover:underline underline-offset-8" href="#search">Abouts Us</a>
+                        <svg className="w-4 mr-40 h-4 text-gray-100 dark:text-gray-100" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                        </svg>
                     </nav>
-                    <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
-                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
-                        <path d="M5 12h14M12 5l7 7-7 7"></path>
-                    </svg>
-                    </button>
+
+                    
+
                 </div>
-            </header>
+            </header> 
             
         </>
     )
