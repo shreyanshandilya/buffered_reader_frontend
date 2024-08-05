d085fd21fd567363ee436fcf6c46d357a0ccbc11
anirban2005143a <23je0104@iitism.ac.in>
2024-08-06 01:44:09 +0530

    fvgfgt

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { HashRouter as Router , Routes, Route } from "react-router-dom"
 import Home from "./components/home"
 import About from "./components/about"
 import Purpose from './components/landing page/purpose'
@@ -22,7 +23,14 @@ function App() {
   ])
 
   return (
-    <RouterProvider router={router} />
+    // <RouterProvider router={router} />
+    <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/purpose" element={<Purpose />} />
+    </Routes>
+  </Router>
   )
 }
 
