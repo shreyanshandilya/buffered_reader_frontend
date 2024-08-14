c43b2a86598dbffc0ff361845de114c7d864fbc1
anirban2005143a <23je0104@iitism.ac.in>
2024-08-14 14:58:55 +0530

    fgtT

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ import { HashRouter as Router , Routes, Route } from "react-router-dom"
 import Home from "./components/home"
 import About from "./components/about"
 import Purpose from './components/landing page/purpose'
-import Footer from "./components/Footer"
+import Editions from "./components/editios/editions"
 
 function App() {
 
   return (
-    // <RouterProvider router={router} />
     <Router>
     <Routes>
       <Route path="/" element={<><Home /></>} />
       <Route path="/about" element={<><About /></>} />
       <Route path="/purpose" element={<Purpose />} />
+      <Route path="/editions" element={<Editions />} />
     </Routes>
   </Router>
   )
