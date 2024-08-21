a0add82d97bb719014d89f0be62396fddd074036
Shashwat <shash01nautiyal@gmail.com>
2024-08-21 16:51:57 +0530

    About us content added, and some minor changes made in teams.

--- src/components/Teams.jsx
+++ src/components/Teams.jsx
@@ -1,35 +1,10 @@
 import React from "react";
 import Navbar from "./navbar";
 import Footer from "./Footer";
-import waveImg from '/wave.png'
+import waveImg from "/wave.png";
 
 const Teams = () => {
   return (
-    // <div>
-    //   <Navbar/>
-    //   <section className="text-gray-600 body-font">
-    //     {/* <div className='h-1/3'>
-    //     <img src="/eth.jpeg" alt="bg-img" className='h-64 w-screen md: ' />
-    //     </div> */}
-    //     <div className=" pt-14 mt-5 pb-5 shadow-2xl rounded-xl  bg-gradient-to-br from-[#a4fbea] to-[#56b1d9] shadow-slate-500 flex md:flex-row flex-col md:justify-center items-center px-3">
-    //                 <img src='/team.jpg' className=" rounded heading sm:w-4/12 w-full sm:my-6 h-1/4"/>
-    //                 <div className=" md:w-5/12 sm:w-9/12 w-full mx-6 my-4 overflow-hidden">
-    //                     <div className='slide flex justify-between items-center' style={{width:"400%"}}>
-    //                          <div className=" w-1/4 mx-4">
-    //                                 <div className="title text-xl font-[700] my-6">Meet Our Amazing Team</div>
-    //                                 <div className="content text-base font-[400] ">
-    //                                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque similique deleniti voluptates tempore voluptas fuga neque, cupiditate id laboriosam quo quisquam esse ea illum delectus culpa corrupti recusandae ipsa amet.
-    //                                 </div>
-    //                          </div>
-
-    //                     </div>
-
-    //                 </div>
-    //     </div>
-
-    //   </section>
-    //   <Footer/>
-    // </div>
     <>
       <div className=" absolute top-0 left-0 w-full">
         <Navbar />
@@ -60,123 +35,200 @@ const Teams = () => {
             }}
           ></div>
         </div>
-        <div className="alleditons">
+        <section className="text-gray-600 body-font">
           <div className="container px-5 py-24 mx-auto">
+            <div className="flex flex-col text-center w-full mb-20">
+              <h1 className="text-2xl font-medium title-font mb-4 text-gray-900">
+                OUR TEAM
+              </h1>
+              <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+              "Individually, we are one drop. Together, we are an ocean."
+              </p>
+            </div>
             <div className="flex flex-wrap -m-4">
-              <div className="p-8 md:w-1/3">
-                <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+              <div className="p-4 lg:w-1/3 md:w-1/2">
+                <div className="h-full flex flex-col items-center text-center">
                   <img
-                    className="lg:h-60 md:h-36 w-full object-cover object-center"
-                    src="/eth.jpeg"
-                    alt="blog"
+                    alt="team"
+                    className="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4"
+                    src="https://dummyimage.com/200x200"
                   />
-                  <div className="p-6">
-                    <h2 className="tracking-widest text-xs text-center title-font font-medium text-gray-400 mb-3">
-                      CATEGORY
+                  <div className="w-full">
+                    <h2 className="title-font font-medium text-lg text-gray-900">
+                    Spandan Kundu
                     </h2>
-                    <h1 className="title-font text-2xl font-medium text-gray-900 mb-1">
-                      The Catalyzer
-                    </h1>
-                    <h2 className="tracking-widest text-lg title-font font-medium text-gray-400 mb-1">
-                      Work Done
-                    </h2>
-                    <div className="flex items-center flex-wrap ">
-                      <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
-                        Linked In
+                    <h3 className="text-gray-500 mb-3">UI Developer</h3>
+                    <p className="mb-4">
+                      DIY tote bag drinking vinegar cronut adaptogen squid fanny
+                      pack vaporware.
+                    </p>
+                    <span className="inline-flex">
+                      <a className="text-gray-500">
                         <svg
-                          className="w-4 h-4 ml-2"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                          strokeWidth="2"
                           fill="none"
+                          stroke="currentColor"
                           strokeLinecap="round"
                           strokeLinejoin="round"
+                          strokLidth="2"
+                          className="w-5 h-5 hover:fill-gray-500"
+                          viewBox="0 0 24 24"
                         >
-                          <path d="M5 12h14"></path>
-                          <path d="M12 5l7 7-7 7"></path>
+                          <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                         </svg>
                       </a>
-                    </div>
+                      <a className="ml-2 text-gray-500">
+                        <svg
+                          fill="none"
+                          stroke="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          className="w-5 h-5  hover:fill-gray-500"
+                          viewBox="0 0 24 24"
+                        >
+                          <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                        </svg>
+                      </a>
+                      <a className="ml-2 text-gray-500">
+                        <svg
+                          fill="none"
+                          stroke="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          className="w-5 h-5 hover:fill-gray-500"
+                          viewBox="0 0 24 24"
+                        >
+                          <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                        </svg>
+                      </a>
+                    </span>
                   </div>
                 </div>
               </div>
-              <div className="p-8 md:w-1/3">
-                <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+              <div className="p-4 lg:w-1/3 md:w-1/2">
+                <div className="h-full flex flex-col items-center text-center">
                   <img
-                    className="lg:h-60 md:h-36 w-full object-cover object-center"
-                    src="/eth.jpeg"
-                    alt="blog"
+                    alt="team"
+                    className="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4"
+                    src="https://dummyimage.com/200x200"
                   />
-                  <div className="p-6">
-                    <h2 className="tracking-widest text-xs text-center title-font font-medium text-gray-400 mb-3">
-                      CATEGORY
+                  <div className="w-full">
+                    <h2 className="title-font font-medium text-lg text-gray-900">
+                    Spandan Kundu
                     </h2>
-                    <h1 className="title-font text-2xl font-medium text-gray-900 mb-1">
-                      The Catalyzer
-                    </h1>
-                    <h2 className="tracking-widest text-lg title-font font-medium text-gray-400 mb-1">
-                      Work Done
-                    </h2>
-                    <div className="flex items-center flex-wrap ">
-                      <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
-                        Linked In
+                    <h3 className="text-gray-500 mb-3">UI Developer</h3>
+                    <p className="mb-4">
+                      DIY tote bag drinking vinegar cronut adaptogen squid fanny
+                      pack vaporware.
+                    </p>
+                    <span className="inline-flex">
+                      <a className="text-gray-500">
                         <svg
-                          className="w-4 h-4 ml-2"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                          strokeWidth="2"
                           fill="none"
+                          stroke="currentColor"
                           strokeLinecap="round"
                           strokeLinejoin="round"
+                          strokLidth="2"
+                          className="w-5 h-5  hover:fill-gray-500"
+                          viewBox="0 0 24 24"
                         >
-                          <path d="M5 12h14"></path>
-                          <path d="M12 5l7 7-7 7"></path>
+                          <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                         </svg>
                       </a>
-                    </div>
+                      <a className="ml-2 text-gray-500">
+                        <svg
+                          fill="none"
+                          stroke="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          className="w-5 h-5  hover:fill-gray-500"
+                          viewBox="0 0 24 24"
+                        >
+                          <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                        </svg>
+                      </a>
+                      <a className="ml-2 text-gray-500">
+                        <svg
+                          fill="none"
+                          stroke="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          className="w-5 h-5  hover:fill-gray-500"
+                          viewBox="0 0 24 24"
+                        >
+                          <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                        </svg>
+                      </a>
+                    </span>
                   </div>
                 </div>
               </div>
-              <div className="p-8 md:w-1/3">
-                <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+              <div className="p-4 lg:w-1/3 md:w-1/2">
+                <div className="h-full flex flex-col items-center text-center">
                   <img
-                    className="lg:h-60 md:h-36 w-full object-cover object-center"
-                    src="/eth.jpeg"
-                    alt="blog"
+                    alt="team"
+                    className="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4"
+                    src="https://dummyimage.com/200x200"
                   />
-                  <div className="p-6">
-                    <h2 className="tracking-widest text-xs text-center title-font font-medium text-gray-400 mb-3">
-                      CATEGORY
+                  <div className="w-full">
+                    <h2 className="title-font font-medium text-lg text-gray-900">
+                      Spandan Kundu
                     </h2>
-                    <h1 className="title-font text-2xl font-medium text-gray-900 mb-1">
-                      The Catalyzer
-                    </h1>
-                    <h2 className="tracking-widest text-lg title-font font-medium text-gray-400 mb-1">
-                      Work Done
-                    </h2>
-                    <div className="flex items-center flex-wrap ">
-                      <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
-                        Linked In
+                    <h3 className="text-gray-500 mb-3">Secretary </h3>
+                    <p className="mb-4">
+                      DIY tote bag drinking vinegar cronut adaptogen squid fanny
+                      pack vaporware.
+                    </p>
+                    <span className="inline-flex">
+                      <a className="text-gray-500">
                         <svg
-                          className="w-4 h-4 ml-2"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                          strokeWidth="2"
                           fill="none"
+                          stroke="currentColor"
                           strokeLinecap="round"
                           strokeLinejoin="round"
+                          strokLidth="2"
+                          className="w-5 h-5 hover:fill-gray-500"
+                          viewBox="0 0 24 24"
                         >
-                          <path d="M5 12h14"></path>
-                          <path d="M12 5l7 7-7 7"></path>
+                          <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                         </svg>
                       </a>
-                    </div>
+                      <a className="ml-2 text-gray-500">
+                        <svg
+                          fill="none"
+                          stroke="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          className="w-5 h-5 hover:fill-gray-500"
+                          viewBox="0 0 24 24"
+                        >
+                          <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                        </svg>
+                      </a>
+                      <a className="ml-2 text-gray-500">
+                        <svg
+                          fill="none"
+                          stroke="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          className="w-5 h-5 hover:fill-gray-500"
+                          viewBox="0 0 24 24"
+                        >
+                          <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                        </svg>
+                      </a>
+                    </span>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        </section>
       </div>
       <Footer />
     </>
--- src/components/landing page/hero.jsx
+++ src/components/landing page/hero.jsx
@@ -41,7 +41,7 @@ const Hero = (props) => {
                             <Cursor className='text-4xl' cursorBlinking={false} cursorStyle='..'/>
                           </span>
                       </div>
-                      <div className='z-50 top-9 right-7 pt-5 fixed'><button type="button" class="sticky text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Latest Edition</button></div>
+                      <div className='z-50 top-20 right-7  fixed'><button type="button" class="sticky text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Latest Edition</button></div>
                       </div>
                     {/* <div className='text-2xl text-gray-800 '>CSE Society</div>
                     <div className='text-2xl text-gray-800 '>Buffered Reader</div> */}
