a037eee55f1ed1b364969ed38972c28d6d6e62ba
anirban2005143a <23je0104@iitism.ac.in>
2024-08-24 16:35:47 +0530

    changed

--- src/components/Teams.jsx
+++ src/components/Teams.jsx
@@ -16,7 +16,8 @@ const Teams = () => {
             height: `${window.innerHeight * 0.7}px`,
             backgroundImage:
               "linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(228,218,137,1) 28%, rgba(0,212,255,1) 100%)",
-          }}
+          maxHeight:"400px"
+            }}
         >
           <div className="heading capitalize text-4xl font-sans font-[700] text-teal-950  ">
             Who Are We ?
@@ -45,7 +46,7 @@ const Teams = () => {
               "Individually, we are one drop. Together, we are an ocean."
               </p>
             </div>
-            <div className="flex flex-wrap -m-4">
+            <div className="flex flex-wrap -m-4 justify-center">
 
 
               <div className="p-4 lg:w-1/3 md:w-1/2">
@@ -78,31 +79,6 @@ const Teams = () => {
                   <img
                     alt="team"
                     className=" border-2 border-slate-900 flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4"
-                    src="sv.jpg"
-                  />
-                  <div className="w-full">
-                    <h2 className="title-font font-medium text-lg text-gray-900">
-                    Shubham Varshney
-                    </h2>
-                    <h3 className="text-gray-500 mb-3">Vice-President  </h3>
-                    <p className="mb-4">
-                      DIY tote bag drinking vinegar cronut adaptogen squid fanny
-                      pack vaporware.
-                    </p>
-                    <span className="inline-flex">
-                      <a href="https://www.linkedin.com/in/shubham0920/" className="text-gray-500">
-                      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 26 26" width="35px" height="35px"><g><path fill="#000000" d="M21.125,0H4.875C2.182,0,0,2.182,0,4.875v16.25C0,23.818,2.182,26,4.875,26h16.25   C23.818,26,26,23.818,26,21.125V4.875C26,2.182,23.818,0,21.125,0z M8.039,22.069H4L3.977,9.977h4.039L8.039,22.069z M5.918,8.394   H5.893c-1.318,0-2.171-0.908-2.171-2.042c0-1.159,0.879-2.041,2.222-2.041c1.344,0,2.171,0.882,2.196,2.041   C8.14,7.485,7.287,8.394,5.918,8.394z M22.042,22.07h-4.075v-6.571c0-1.588-0.421-2.671-1.842-2.671   c-1.084,0-1.671,0.731-1.955,1.437c-0.104,0.253-0.13,0.604-0.13,0.957v6.849H9.945L9.922,9.977h4.095l0.023,1.705   c0.521-0.806,1.394-1.953,3.48-1.953c2.584,0,4.521,1.688,4.521,5.317V22.07z"/></g></svg>
-                      </a>
-                    </span>
-                  </div>
-                </div>
-              </div>
-
-              <div className="p-4 lg:w-1/3 md:w-1/2">
-                <div className="h-full flex flex-col items-center text-center">
-                  <img
-                    alt="team"
-                    className=" border-2 border-slate-900 flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4"
                     src="/dr.jpg"
                   />
                   <div className="w-full">
@@ -116,6 +92,31 @@ const Teams = () => {
                     </p>
                     <span className="inline-flex">
                       <a href="https://www.linkedin.com/in/dr-ramesh-dharavath-173572a4/" className="text-gray-500">
+                      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 26 26" width="35px" height="35px"><g><path fill="#000000" d="M21.125,0H4.875C2.182,0,0,2.182,0,4.875v16.25C0,23.818,2.182,26,4.875,26h16.25   C23.818,26,26,23.818,26,21.125V4.875C26,2.182,23.818,0,21.125,0z M8.039,22.069H4L3.977,9.977h4.039L8.039,22.069z M5.918,8.394   H5.893c-1.318,0-2.171-0.908-2.171-2.042c0-1.159,0.879-2.041,2.222-2.041c1.344,0,2.171,0.882,2.196,2.041   C8.14,7.485,7.287,8.394,5.918,8.394z M22.042,22.07h-4.075v-6.571c0-1.588-0.421-2.671-1.842-2.671   c-1.084,0-1.671,0.731-1.955,1.437c-0.104,0.253-0.13,0.604-0.13,0.957v6.849H9.945L9.922,9.977h4.095l0.023,1.705   c0.521-0.806,1.394-1.953,3.48-1.953c2.584,0,4.521,1.688,4.521,5.317V22.07z"/></g></svg>
+                      </a>
+                    </span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="p-4 lg:w-1/3 md:w-1/2">
+                <div className="h-full flex flex-col items-center text-center">
+                  <img
+                    alt="team"
+                    className=" border-2 border-slate-900 flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4"
+                    src="sv.jpg"
+                  />
+                  <div className="w-full">
+                    <h2 className="title-font font-medium text-lg text-gray-900">
+                    Shubham Varshney
+                    </h2>
+                    <h3 className="text-gray-500 mb-3">Vice-President  </h3>
+                    <p className="mb-4">
+                      DIY tote bag drinking vinegar cronut adaptogen squid fanny
+                      pack vaporware.
+                    </p>
+                    <span className="inline-flex">
+                      <a href="https://www.linkedin.com/in/shubham0920/" className="text-gray-500">
                       <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 26 26" width="35px" height="35px"><g><path fill="#000000" d="M21.125,0H4.875C2.182,0,0,2.182,0,4.875v16.25C0,23.818,2.182,26,4.875,26h16.25   C23.818,26,26,23.818,26,21.125V4.875C26,2.182,23.818,0,21.125,0z M8.039,22.069H4L3.977,9.977h4.039L8.039,22.069z M5.918,8.394   H5.893c-1.318,0-2.171-0.908-2.171-2.042c0-1.159,0.879-2.041,2.222-2.041c1.344,0,2.171,0.882,2.196,2.041   C8.14,7.485,7.287,8.394,5.918,8.394z M22.042,22.07h-4.075v-6.571c0-1.588-0.421-2.671-1.842-2.671   c-1.084,0-1.671,0.731-1.955,1.437c-0.104,0.253-0.13,0.604-0.13,0.957v6.849H9.945L9.922,9.977h4.095l0.023,1.705   c0.521-0.806,1.394-1.953,3.48-1.953c2.584,0,4.521,1.688,4.521,5.317V22.07z"/></g></svg>
                       </a>
                     </span>
