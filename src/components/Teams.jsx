9d9bf15dcedc077dd4cb6b5c8625c8520690bee8
Shreyansh Shandilya <shreyanshandilya@gmail.com>
2024-08-30 01:59:36 +0530

    changes

--- src/components/Teams.jsx
+++ src/components/Teams.jsx
@@ -47,7 +47,7 @@ const Teams = () => {
             <div className="flex flex-wrap -m-4 justify-center">
 
 
-              <p className="font-bold text-2xl text-black bg-gradient-to-r from-[ #00c9ff] to-[#92fe9d] rounded-lg px-5 py-3 font-serif">Executive Committee</p><br></br><br></br>
+              <p className="font-bold text-2xl text-black bg-gradient-to-r from-[ #00c9ff] to-[#92fe9d] rounded-lg px-5 py-3 font-serif">BufferedAdmin</p><br></br><br></br>
               <div className="flex flex-wrap -m-4 mb-12 justify-center pt-3">
                 <div className="p-4 lg:w-1/3 md:w-1/2">
                   <div className="h-full text-center">
@@ -60,7 +60,6 @@ const Teams = () => {
                       <h2 className="title-font font-medium text-lg text-gray-900">
                         Prof. Chiranjeev Kumar
                       </h2>
-                      <h3 className="text-gray-500 mb-3">President</h3>
                       {/* <p className="mb-4">
                         DIY tote bag drinking vinegar cronut adaptogen squid fanny
                         pack vaporware.
@@ -78,13 +77,12 @@ const Teams = () => {
                     <img
                       alt="team"
                       className=" object-cover shadow-xl shadow-stone-400 flex-shrink-0 rounded-lg w-full h-4/6 object-center mb-4"
-                      src="/dr.jpg"
+                      src="/sb_sir.jpg"
                     />
                     <div className="w-full">
                       <h2 className="title-font font-medium text-lg text-gray-900">
-                        Prof. Dharavath Ramesh
+                        Prof. Soumen Bag
                       </h2>
-                      <h3 className="text-gray-500 mb-3">Faculty Incharge </h3>
                       {/* <p className="mb-4">
                         DIY tote bag drinking vinegar cronut adaptogen squid fanny
                         pack vaporware.
@@ -102,13 +100,12 @@ const Teams = () => {
                     <img
                       alt="team"
                       className=" object-cover shadow-xl shadow-stone-400 flex-shrink-0 rounded-lg w-full h-4/6 object-center mb-4"
-                      src="/ps.jpg"
+                      src="/pb_coFIC.jpg"
                     />
                     <div className="w-full">
                       <h2 className="title-font font-medium text-lg text-gray-900">
-                        Prof. Pranay Kumar Saha
+                        Prof. Pranav Bisht
                       </h2>
-                      <h3 className="text-gray-500 mb-3">Treasurer</h3>
                       {/* <p className="mb-4">
                         DIY tote bag drinking vinegar cronut adaptogen squid fanny
                         pack vaporware.
@@ -118,133 +115,6 @@ const Teams = () => {
                           <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 26 26" width="35px" height="35px"><g><path fill="#000000" d="M21.125,0H4.875C2.182,0,0,2.182,0,4.875v16.25C0,23.818,2.182,26,4.875,26h16.25   C23.818,26,26,23.818,26,21.125V4.875C26,2.182,23.818,0,21.125,0z M8.039,22.069H4L3.977,9.977h4.039L8.039,22.069z M5.918,8.394   H5.893c-1.318,0-2.171-0.908-2.171-2.042c0-1.159,0.879-2.041,2.222-2.041c1.344,0,2.171,0.882,2.196,2.041   C8.14,7.485,7.287,8.394,5.918,8.394z M22.042,22.07h-4.075v-6.571c0-1.588-0.421-2.671-1.842-2.671   c-1.084,0-1.671,0.731-1.955,1.437c-0.104,0.253-0.13,0.604-0.13,0.957v6.849H9.945L9.922,9.977h4.095l0.023,1.705   c0.521-0.806,1.394-1.953,3.48-1.953c2.584,0,4.521,1.688,4.521,5.317V22.07z" /></g></svg>
                         </a>
                       </span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <p className="font-bold text-2xl text-black bg-gradient-to-r from-[ #00c9ff] to-[#92fe9d] rounded-lg px-5 py-3 font-serif">Editorial Committee</p><br></br><br></br>
-              <div className="flex flex-wrap -m-4 mb-12 justify-center pt-3">
-                <div className="p-4 lg:w-1/3 md:w-1/2">
-                  <div className="h-full text-center">
-                    <img
-                      alt="team"
-                      className=" object-cover shadow-xl shadow-stone-400 flex-shrink-0 rounded-lg w-full h-4/6 object-center mb-4"
-                      src="sv.jpg"
-                    />
-                    <div className="w-full">
-                      <h2 className="title-font font-medium text-lg text-gray-900">
-                        Shubham Varshney
-                      </h2>
-                      <h3 className="text-gray-500 mb-3">Vice-President  </h3>
-                      {/* <p className="mb-4">
-                        DIY tote bag drinking vinegar cronut adaptogen squid fanny
-                        pack vaporware.
-                      </p> */}
-                      <span className="inline-flex">
-                        <a href="https://www.linkedin.com/in/shubham0920/" className="text-gray-500">
-                          <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 26 26" width="35px" height="35px"><g><path fill="#000000" d="M21.125,0H4.875C2.182,0,0,2.182,0,4.875v16.25C0,23.818,2.182,26,4.875,26h16.25   C23.818,26,26,23.818,26,21.125V4.875C26,2.182,23.818,0,21.125,0z M8.039,22.069H4L3.977,9.977h4.039L8.039,22.069z M5.918,8.394   H5.893c-1.318,0-2.171-0.908-2.171-2.042c0-1.159,0.879-2.041,2.222-2.041c1.344,0,2.171,0.882,2.196,2.041   C8.14,7.485,7.287,8.394,5.918,8.394z M22.042,22.07h-4.075v-6.571c0-1.588-0.421-2.671-1.842-2.671   c-1.084,0-1.671,0.731-1.955,1.437c-0.104,0.253-0.13,0.604-0.13,0.957v6.849H9.945L9.922,9.977h4.095l0.023,1.705   c0.521-0.806,1.394-1.953,3.48-1.953c2.584,0,4.521,1.688,4.521,5.317V22.07z" /></g></svg>
-                        </a>
-                      </span>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="p-4 lg:w-1/3 md:w-1/2">
-                  <div className="h-full text-center">
-                    <img
-                      alt="team"
-                      className=" object-cover shadow-xl shadow-stone-400 flex-shrink-0 rounded-lg w-full h-4/6 object-center mb-4"
-                      src="/sk.jpg"
-                    />
-                    <div className="w-full">
-                      <h2 className="title-font font-medium text-lg text-gray-900">
-                        Spandan Kundu
-                      </h2>
-                      <h3 className="text-gray-500 mb-3">Secretary </h3>
-                      {/* <p className="mb-4">
-                        DIY tote bag drinking vinegar cronut adaptogen squid fanny
-                        pack vaporware.
-                      </p> */}
-                      <span className="inline-flex">
-                        <a href="https://www.linkedin.com/in/spandan-kundu-22582822b/" className="text-gray-500">
-                          <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 26 26" width="35px" height="35px"><g><path fill="#000000" d="M21.125,0H4.875C2.182,0,0,2.182,0,4.875v16.25C0,23.818,2.182,26,4.875,26h16.25   C23.818,26,26,23.818,26,21.125V4.875C26,2.182,23.818,0,21.125,0z M8.039,22.069H4L3.977,9.977h4.039L8.039,22.069z M5.918,8.394   H5.893c-1.318,0-2.171-0.908-2.171-2.042c0-1.159,0.879-2.041,2.222-2.041c1.344,0,2.171,0.882,2.196,2.041   C8.14,7.485,7.287,8.394,5.918,8.394z M22.042,22.07h-4.075v-6.571c0-1.588-0.421-2.671-1.842-2.671   c-1.084,0-1.671,0.731-1.955,1.437c-0.104,0.253-0.13,0.604-0.13,0.957v6.849H9.945L9.922,9.977h4.095l0.023,1.705   c0.521-0.806,1.394-1.953,3.48-1.953c2.584,0,4.521,1.688,4.521,5.317V22.07z" /></g></svg>
-                        </a>
-                      </span>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="p-4 lg:w-1/3 md:w-1/2">
-                  <div className="h-full text-center">
-                    <img
-                      alt="team"
-                      className=" object-cover shadow-xl shadow-stone-400 flex-shrink-0 rounded-lg w-full h-4/6 object-center mb-4"
-                      src="/rk.jpg"
-                    />
-                    <div className="w-full">
-                      <h2 className="title-font font-medium text-lg text-gray-900">
-                        Ram Krishna
-                      </h2>
-                      <h3 className="text-gray-500 mb-3">Joint-Secretary  </h3>
-                      {/* <p className="mb-4">
-                        DIY tote bag drinking vinegar cronut adaptogen squid fanny
-                        pack vaporware.
-                      </p> */}
-                      <span className="inline-flex">
-                        <a href="https://www.linkedin.com/in/ram-krishna-bbba8626a/" className="text-gray-500">
-                          <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 26 26" width="35px" height="35px"><g><path fill="#000000" d="M21.125,0H4.875C2.182,0,0,2.182,0,4.875v16.25C0,23.818,2.182,26,4.875,26h16.25   C23.818,26,26,23.818,26,21.125V4.875C26,2.182,23.818,0,21.125,0z M8.039,22.069H4L3.977,9.977h4.039L8.039,22.069z M5.918,8.394   H5.893c-1.318,0-2.171-0.908-2.171-2.042c0-1.159,0.879-2.041,2.222-2.041c1.344,0,2.171,0.882,2.196,2.041   C8.14,7.485,7.287,8.394,5.918,8.394z M22.042,22.07h-4.075v-6.571c0-1.588-0.421-2.671-1.842-2.671   c-1.084,0-1.671,0.731-1.955,1.437c-0.104,0.253-0.13,0.604-0.13,0.957v6.849H9.945L9.922,9.977h4.095l0.023,1.705   c0.521-0.806,1.394-1.953,3.48-1.953c2.584,0,4.521,1.688,4.521,5.317V22.07z" /></g></svg>
-                        </a>
-                      </span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <p className="font-bold text-2xl  text-black bg-gradient-to-r from-[ #00c9ff] to-[#92fe9d]rounded-lg px-5 py-3 font-serif">BufferedDevelopers</p><br></br><br></br>
-              <div className="flex flex-wrap -m-4 mb-12 justify-center pt-3">
-                <div className="p-4 lg:w-1/3 md:w-1/2">
-                  <div className="h-full text-center">
-                    <img
-                      alt="team"
-                      className=" object-cover shadow-xl shadow-stone-400 flex-shrink-0 rounded-lg w-full h-4/6 object-center mb-4"
-                      src="/shreyansh.jpeg"
-                    />
-                    <div className="w-full">
-                      <h2 className="title-font font-medium text-lg text-gray-900">
-                      Shreyansh Shandilya
-                      </h2>
-                      <h3 className="text-gray-500 mb-3"> (Lead Developer)  </h3>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="p-4 lg:w-1/3 md:w-1/2">
-                  <div className="h-full text-center">
-                    <img
-                      alt="team"
-                      className=" object-cover shadow-xl shadow-stone-400 flex-shrink-0 rounded-lg w-full h-4/6 object-center mb-4"
-                      src="/anirban.jpeg"
-                    />
-                    <div className="w-full">
-                      <h2 className="title-font font-medium text-lg text-gray-900">
-                        Anirban Das
-                      </h2>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="p-4 lg:w-1/3 md:w-1/2">
-                  <div className="h-full text-center">
-                    <img
-                      alt="team"
-                      className=" object-cover shadow-xl shadow-stone-400 flex-shrink-0 rounded-lg w-full h-4/6 object-center mb-4"
-                      src="/Shashwat.jpg"
-                    />
-                    <div className="w-full">
-                      <h2 className="title-font font-medium text-lg text-gray-900">
-                      Shashwat Nautiyal
-                      </h2>
                     </div>
                   </div>
                 </div>
@@ -584,7 +454,54 @@ const Teams = () => {
                   </div>
                 </div>
 
-                
+                <p className="font-bold text-2xl  text-black bg-gradient-to-r from-[ #00c9ff] to-[#92fe9d]rounded-lg px-5 py-3 font-serif">BufferedDevelopers</p><br></br><br></br>
+              <div className="flex flex-wrap -m-4 mb-12 justify-center pt-3">
+                <div className="p-4 lg:w-1/3 md:w-1/2">
+                  <div className="h-full text-center">
+                    <img
+                      alt="team"
+                      className=" object-cover shadow-xl shadow-stone-400 flex-shrink-0 rounded-lg w-full h-4/6 object-center mb-4"
+                      src="/shreyansh.jpeg"
+                    />
+                    <div className="w-full">
+                      <h2 className="title-font font-medium text-lg text-gray-900">
+                      Shreyansh Shandilya
+                      </h2>
+                      <h3 className="text-gray-500 mb-3"> (Lead Developer)  </h3>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="p-4 lg:w-1/3 md:w-1/2">
+                  <div className="h-full text-center">
+                    <img
+                      alt="team"
+                      className=" object-cover shadow-xl shadow-stone-400 flex-shrink-0 rounded-lg w-full h-4/6 object-center mb-4"
+                      src="/anirban.jpeg"
+                    />
+                    <div className="w-full">
+                      <h2 className="title-font font-medium text-lg text-gray-900">
+                        Anirban Das
+                      </h2>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="p-4 lg:w-1/3 md:w-1/2">
+                  <div className="h-full text-center">
+                    <img
+                      alt="team"
+                      className=" object-cover shadow-xl shadow-stone-400 flex-shrink-0 rounded-lg w-full h-4/6 object-center mb-4"
+                      src="/Shashwat.jpg"
+                    />
+                    <div className="w-full">
+                      <h2 className="title-font font-medium text-lg text-gray-900">
+                      Shashwat Nautiyal
+                      </h2>
+                    </div>
+                  </div>
+                </div>
+              </div>
 
               </div>
             </div>
