f2388f68d30e69ec362f4d0f2948e6642c722237
anirban2005143a <23je0104@iitism.ac.in>
2024-08-29 19:29:19 +0530

    dfGRR

--- src/components/landing page/purpose.jsx
+++ src/components/landing page/purpose.jsx
@@ -27,7 +27,8 @@ const purpose = (props) => {
         <>
             <div className={`p-3 position-relative transition-all duration-700  ${props.isPurposeVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-32"} `} id="purpose" ref={props.purposeRef}>
 
-                <div className=" py-8 shadow-2xl rounded-xl bg-[#f2f1e3] shadow-[#918b7c] flex md:flex-row flex-col md:justify-center items-center px-3">
+                <div className=" py-8 shadow-2xl rounded-xl  shadow-[#918b7c] flex md:flex-row flex-col md:justify-center items-center px-3"
+                style={{backgroundImage:"linear-gradient(to right, #2bc0e4, #eaecc6)"}}>
                     <div className="heading sm:w-4/12 w-full text-3xl font-[800] uppercase sm:my-6 text-center">Purpose</div>
                     <div className=" md:w-5/12 sm:w-9/12 w-full mx-6 my-4 overflow-hidden">
                         <div className='slide flex justify-between items-center' style={{width:"400%"}}>
